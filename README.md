195fed66ed3a4ba53963df7421936b1eac4f6c1f
oittaa <eero@oittaa.net>
2019-01-31 15:00:30 +0200

    Use cache for Signal APK files. Remove python3-wget dependeny. APK location is hard coded in the test.

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # reproducible-signal
+
+[![Build Status](https://travis-ci.org/oittaa/reproducible-signal.svg)](https://travis-ci.org/oittaa/reproducible-signal)
+
+Since version 3.15.0 Signal for Android has supported reproducible builds. This is achieved by replicating the build environment as a Docker image. You'll need to build the image, run a container instance of it, compile Signal inside the container and finally compare the resulted APK to the APK that is distributed in the Google Play Store.
+
+## TL;DR
+
+1. [Enable developer options and USB debugging from your phone](https://developer.android.com/studio/debug/dev-options#enable)
+2. Connect your phone to the computer via USB
+3. run `./reproducible-signal.sh`
--- test/test_reproducible-signal.py
+++ test/test_reproducible-signal.py
@@ -1,20 +1,17 @@
 #! /usr/bin/env python3
 
-import json
+import os
+from pathlib import Path
 import subprocess
-import urllib.request
-import wget
 
-SIGNAL_APP_JSON="https://updates.signal.org/android/latest.json"
+SIGNAL_APK=os.path.join(Path.home(), "Signal.apk")
 
 def test_reproducible_signal():
-    data = json.loads(urllib.request.urlopen(SIGNAL_APP_JSON).read())
-    apk_filename = wget.download(data['url'])
     popen = subprocess.Popen(
-        ["./reproducible-signal.sh", apk_filename],
+        ["./reproducible-signal.sh", SIGNAL_APK],
         stdout=subprocess.PIPE, universal_newlines=True, bufsize=1
     )
     for line in popen.stdout:
-        current_line = line.strip()
+        current_line = line.rstrip()
         print(current_line)
     assert current_line == "APKs match!"
