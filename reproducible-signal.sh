69dd5a2764f19c41038d5ffa65058507b24f317a
oittaa <eero@oittaa.net>
2019-02-01 16:14:27 +0200

    Only set exit status 0 if the APKs matched. Wait for authorization from the connected phone. Minor text changes. Refactoring.

--- reproducible-signal.sh
+++ reproducible-signal.sh
@@ -14,28 +14,11 @@ display_help() {
 	printf >&2 "\thttps://developer.android.com/studio/debug/dev-options#enable\n\n"
 	printf >&2 "\tAlternatively as the first parameter you can submit an APK that\n"
 	printf >&2 "\twas previously extracted.\n\n"
-	printf >&2 "\tIf the script finishes successfully, the last line should read:\n"
-	printf >&2 "\t\"APKs match!\"\n"
-	printf >&2 "\tDon't worry about the \"BUILD FAILED\" message you'll see above.\n"
+	printf >&2 "\tIf the script finishes successfully and the APKs match, the last \n"
+	printf >&2 "\tline should read \"APKs match!\" and exit status is set to \"0\".\n"
+	printf >&2 "\tDon't worry about the \"BUILD FAILED\" message you'll see above it.\n"
 	printf >&2 "\tYou don't have the signing key, but the unsigned APK was built anyway.\n"
 	exit 1
-}
-
-check_connected_devices() {
-	# Check if the phone is connected
-	printf "##### Trying to find a connected phone.\n"
-	count=0
-	while ! adb devices -l | grep -P '^[A-Z0-9]{5,}'
-	do
-		count=$((count+1))
-		if [ "$count" -ge 100 ]
-		then
-			printf >&2 "Timed out. Aborting.\n"
-			exit 1
-		fi
-		printf "%s Coudn't find any connected Android devices. Waiting...\n" "$(date "+%F %T")"
-		sleep 3
-	done
 }
 
 display_disconnect_device() {
@@ -43,12 +26,18 @@ display_disconnect_device() {
 	if [ "$DISPLAY" ] || [ "$WAYLAND_DISPLAY" ] || [ "$MIR_SOCKET" ] && command -v zenity >/dev/null 2>&1
 	then
 		zenity --info --timeout 60 --title="Signal APK extracted" --height 150 --width 400 \
-			--text="<big>You can disconnect your phone now.</big>\n\nThis window closes automatically after 60 seconds. The extracted APK can be found at:\n${APK_DIR_FROM_PLAY_STORE}/${APK_FILE_FROM_PLAY_STORE}" &
+			--text="<big>You can disconnect your phone now.</big>\n\nThis window closes automatically after 60 seconds.\n\nThe extracted APK can be found at ${APK_DIR_FROM_PLAY_STORE}/${APK_FILE_FROM_PLAY_STORE}" &
 	else
 		printf "#####################################################################\n"
 		printf "#####\t\tYOU CAN DISCONNECT YOUR PHONE NOW\t\t#####\n"
 		printf "#####################################################################\n"
 	fi
+}
+
+cleanup() {
+  rv=$?
+  rm -f "$LOGFILE"
+  exit $rv
 }
 
 if [ "$1" = "-h" ] || [ "$1" = "--help" ]
@@ -72,10 +61,33 @@ then
 	APK_DIR_FROM_PLAY_STORE=$(dirname "$(realpath "$1")")
 elif [ -z "$1" ]
 then
-	check_connected_devices
+	count=0
+	# Check if the phone is connected
+	printf "##### Trying to find a connected phone.\n"
+	while ! adb devices -l | grep -P '^[A-Z0-9]{5,}'
+	do
+		count=$((count+1))
+		if [ "$count" -ge 100 ]
+		then
+			printf >&2 "Timed out. Aborting.\n"
+			exit 1
+		fi
+		printf "%s Coudn't find any connected Android devices. Waiting...\n" "$(date "+%F %T")"
+		sleep 3
+	done
 	# Try to fetch the apk from the phone
 	printf "##### Fetching the APK from the phone.\n"
-	APK_PATH=$(adb shell pm path org.thoughtcrime.securesms | grep -oP '^package:\K.*/base.apk$')
+	while ! APK_PATH=$(adb shell pm path org.thoughtcrime.securesms | grep -oP '^package:\K.*/base.apk$')
+	do
+		count=$((count+1))
+		if [ "$count" -ge 100 ]
+		then
+			printf >&2 "Timed out. Aborting.\n"
+			exit 1
+		fi
+		printf "Waiting for authorization...\n"
+		sleep 3
+	done
 	APK_FILE_FROM_PLAY_STORE="Signal-$(date '+%F_%T').apk"
 	adb pull \
     	"${APK_PATH}" \
@@ -98,6 +110,8 @@ docker build --file Dockerfile_v${VERSION} --tag signal-android .
 
 printf "##### Compiling Signal inside a container.\n"
 printf "##### This will take some time!\n"
+LOGFILE=$(mktemp --tmpdir reproducible-signal.XXXXXXXXXX.log)
+trap cleanup EXIT HUP INT QUIT ABRT TERM
 docker run \
 	--name signal \
 	--rm \
@@ -108,4 +122,7 @@ docker run \
 		&& chmod +x apkdiff3.py && git clone https://github.com/signalapp/Signal-Android.git \
 		&& cd Signal-Android && git checkout --quiet v${VERSION} && ./gradlew clean assembleRelease \
 		; ../apkdiff3.py build/outputs/apk/play/release/Signal-play-release-unsigned-${VERSION}.apk \
-			'../apk-from-google-play-store/${APK_FILE_FROM_PLAY_STORE}'"
+			'../apk-from-google-play-store/${APK_FILE_FROM_PLAY_STORE}'" | tee "$LOGFILE"
+
+# Set exit status
+tail -n 1 "$LOGFILE" | grep -q "^APKs match"
